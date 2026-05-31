0d013679e26aa43d7714f01e24206fc3598e7132
Akash-G3 <thisisakash481@gmail.com>
2026-05-31 10:27:18 +0000

    Worked on logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="select-none">
-      <h1 className="text-5xl text-neutral-800 font-bold tracking-[0.10em] text-black">
+      <h1 className="text-5xl text-neutral-800 font-bold tracking-[0.10em] text-neutral-800">
         ui<span className="text-orange-500">.lab</span>
       </h1>
     </div>
